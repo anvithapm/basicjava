a5bffb89130978be9d3e3ca14a0c8f330564bf36
anvithapm <anvithapm@users.noreply.github.com>
2017-08-19 17:44:15 -0700

    Add files via upload

---
+++ Application2.java
@@ -0,0 +1,11 @@
+
+public class Application2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car C1 = new Car();
+		C1.start();
+		C1.stop();
+	}
+
+}
---
+++ Car.java
@@ -0,0 +1,11 @@
+
+public class Car {
+
+	void start() {
+		System.out.println("starting the car");
+	}
+
+	void stop() {
+		System.out.println("stopping the car");
+	}
+}
