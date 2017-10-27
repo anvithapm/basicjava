ec0a986177e497ea9c70b8c498d54868f11a604a
anvithapm <anvithapm@users.noreply.github.com>
2017-10-27 16:27:13 -0700

    Add files via upload

---
+++ Application3.java
@@ -0,0 +1,13 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Player P1 = new Player();
+		P1.play();
+		int score = P1.getScore();
+		System.out.println("Your score is " + score);
+		P1.getScore(10);
+	}
+
+}
---
+++ Player1.java
@@ -0,0 +1,13 @@
+
+public class Player {
+	void play() {
+
+	}
+
+	int getScore() {
+		return 100;
+	}
+	void getScore(int gameNumber){
+		System.out.println("Game number is " + gameNumber);
+	}
+}
